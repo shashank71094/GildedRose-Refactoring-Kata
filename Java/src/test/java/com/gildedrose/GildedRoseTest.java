8d8aa7914139d3fc4c1213a0ade3c68f06121e93
Varun Ramachandran <varunram1997@gmail.com>
2021-03-26 13:10:38 +0530

    Update GildedRoseTest.java

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -167,6 +167,58 @@ class GildedRoseTest {
         assertEquals(51, app.items[0].quality);
     }
 
+    // “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+
+    @Test
+    void sulfurasNotExpired() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 10 ) };
+        updateQuality(items);
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(10, app.items[0].quality);
+    }
+
+    @Test
+    void sulfurasExpired() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 10 ) };
+        updateQuality(items);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(10, app.items[0].quality);
+    }
+
+    /*
+    * “Backstage passes”, like aged brie, increases in Quality as its SellIn value approaches
+    *       Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
+    *       Quality drops to 0 after the concert
+    * */
+
+    @Test
+    void backstagePassesFifteenDaysAway() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10 ) };
+        updateQuality(items);
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesTenDaysAway() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10 ) };
+        updateQuality(items);
+        assertEquals(12, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesFiveDaysAway() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10 ) };
+        updateQuality(items);
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesExpired() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10 ) };
+        updateQuality(items);
+        assertEquals(0, app.items[0].quality);
+    }
+
     private void updateQuality(Item[] items) {
         app = new GildedRose(items);
         app.updateQuality();
